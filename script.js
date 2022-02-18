c0c1dae49dd47d019d7127b5c0038bedbf58cb0b
sgruffat <58631777+sgruffat@users.noreply.github.com>
2022-02-18 12:36:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ var models = [
         url: './assets/pair/pair.glb',
         scale: '10 10 10',
         rotation: '0 180 0',
-        info: 'Articuno, Lv. 80, HP 10/10',
+        info: 'Articuno, Lv. 5, HP 10/10',
     },
  
 ];
