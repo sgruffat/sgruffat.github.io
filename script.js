ce9fb303ecd74e43b102b799215b00ad7bdf4777
sgruffat <58631777+sgruffat@users.noreply.github.com>
2022-02-18 15:14:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -20,17 +20,19 @@ function staticLoadPlaces() {
 }
 
 var models = [
-    {
-        url: './assets/magnemite/scene.gltf',
-        scale: '0.5 0.5 0.5',
-        info: 'Magnemite, Lv. 5, HP 10/10',
-        rotation: '0 180 0',
-    },
+ 
     {
         url: './assets/pair/pair.glb',
         scale: '0.5 0.5 0.5',
         rotation: '0 180 0',
         info: 'Articuno, Lv. 5, HP 10/10',
+    },
+    
+       {
+        url: './assets/magnemite/scene.gltf',
+        scale: '0.5 0.5 0.5',
+        info: 'Magnemite, Lv. 5, HP 10/10',
+        rotation: '0 180 0',
     },
  
 ];
